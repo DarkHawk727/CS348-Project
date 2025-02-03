65376b872decde1e722b61c82f9758c9d128b880
Arjun Sarao <arjunsarao727@gmail.com>
2025-02-03 17:52:18 -0500

    pare down database

--- test.py
+++ test.py
@@ -10,7 +10,11 @@ with open("queries/data.sql", "r") as file:
     lines = "".join(file.readlines())
 duckdb.sql(lines)
 
-# Show all the tables and verify that there are 1000 rows.
-with open("queries/check.sql", "r") as file:
-    lines = "".join(file.readlines())
-duckdb.sql(lines)
+duckdb.sql("""
+SELECT * FROM film;
+""").show()
+
+# # Show all the tables and verify that there are 1000 rows.
+# with open("queries/check.sql", "r") as file:
+#     lines = "".join(file.readlines())
+# duckdb.sql(lines)
