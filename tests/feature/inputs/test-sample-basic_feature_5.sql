ceff07ad82c3902f10607424a6a16b052ebd5acd
Hans Wang <hanswang123456@gmail.com>
2025-03-14 19:33:29 -0400

    reorgainzed .out

--- tests/feature/inputs/test-sample-basic_feature_5.sql
+++ tests/feature/inputs/test-sample-basic_feature_5.sql
@@ -1,4 +1,20 @@
- SELECT f.*, l.name as language
-            FROM FILM f
-            LEFT JOIN LANGUAGE l ON f.language_id = l.language_id
-            WHERE f.title = 'ACADEMY DINOSAUR'
+-- this is a basic recommendation query that returns all movies that have at least one actor shared  
+-- and same category as user input query
+
+WITH FilmActors AS (
+    SELECT fa.actor_id
+    FROM FILM_ACTOR fa
+    WHERE fa.film_id = 73 -- user input for the id (will be passed from the Python API)
+),
+FilmCategories AS (
+    SELECT fc.category_id
+    FROM FILM_CATEGORY fc
+    WHERE fc.film_id = 73 -- user input for the id (will be passed from the Python API)
+)
+SELECT DISTINCT f.film_id, f.title
+FROM FILM f
+JOIN FILM_ACTOR fa ON f.film_id = fa.film_id
+JOIN FILM_CATEGORY fc ON f.film_id = fc.film_id
+WHERE 
+    fa.actor_id IN (SELECT actor_id FROM FilmActors) 
+    OR fc.category_id IN (SELECT category_id FROM FilmCategories);
